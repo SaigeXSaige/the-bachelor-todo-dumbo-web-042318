d7cd38ecc20f31df51e3b366eabf6aa04a8010a5
Saige Leslie <saigeleslie@gmail.com>
2018-04-14 15:33:07 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -66,7 +66,6 @@ def get_average_age_for_season(data, season)
   
   data.each do |key, value|
     value.each do |x|
-      binding.pry
       if key == season
         age << x["age"]
         binding.pry
